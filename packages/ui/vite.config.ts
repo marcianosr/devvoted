31e8ead5f427e31bd8009b6884a6d1c5be8bc576
Marciano Schildmeijer <marciano@kabisa.nl>
2024-04-08 14:08:31 +0200

    Comment out vite build.

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -26,26 +26,26 @@ export default defineConfig({
 
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
-  build: {
-    outDir: "../../dist/packages/ui",
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-    lib: {
-      // Could also be a dictionary or array of multiple entry points.
-      entry: "src/index.tsx",
-      name: "ui",
-      fileName: "index",
-      // Change this to the formats you want to support.
-      // Don't forget to update your package.json as well.
-      formats: ["es", "cjs"],
-    },
-    rollupOptions: {
-      // External packages that should not be bundled into your library.
-      external: ["react", "react-dom", "react/jsx-runtime"],
-    },
-  },
+  // build: {
+  //   outDir: "../../dist/packages/ui",
+  //   reportCompressedSize: true,
+  //   commonjsOptions: {
+  //     transformMixedEsModules: true,
+  //   },
+  //   lib: {
+  //     // Could also be a dictionary or array of multiple entry points.
+  //     entry: "src/index.tsx",
+  //     name: "ui",
+  //     fileName: "index",
+  //     // Change this to the formats you want to support.
+  //     // Don't forget to update your package.json as well.
+  //     formats: ["es", "cjs"],
+  //   },
+  //   rollupOptions: {
+  //     // External packages that should not be bundled into your library.
+  //     external: ["react", "react-dom", "react/jsx-runtime"],
+  //   },
+  // },
 
   test: {
     globals: true,
