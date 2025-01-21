31a1c8169a2d239ba09702b3cdeeab039cb7eb56
Marciano Schildmeijer <marciano@kabisa.nl>
2025-01-21 22:39:59 +0100

    feat: add poll responded component

--- src/app/polls/[id]/responses/page.tsx
+++ src/app/polls/[id]/responses/page.tsx
@@ -1,10 +1,9 @@
 import Text from "@/components/ui/Text";
-import { SmallText } from "@/components/ui/Text";
 import Title from "@/components/ui/Title";
 import { Metadata } from "next";
-import { PollSubmissionForm } from "@/components/PollSubmissionForm";
 import { getPoll } from "@/services/polls";
 import PollQuestion from "@/components/PollQuestion";
+
 type Props = {
 	params: { id: string };
 };
@@ -17,7 +16,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 	};
 }
 
-export default async function PollPage({ params }: Props) {
+export default async function PollResponsesPage({ params }: Props) {
 	const poll = await getPoll(params.id);
 
 	if (!poll) {
@@ -31,27 +30,18 @@ export default async function PollPage({ params }: Props) {
 		);
 	}
 
-	const hasResponded = poll.responses.some(
-		(response) => response.userId === "1"
-	);
-
-	if (hasResponded) {
-		return (
-			<div className="container mx-auto px-4 py-8">
-				<Title>You have already responded to this poll</Title>
-				<Text>You can only submit one response per poll.</Text>
-			</div>
-		);
-	}
-
 	return (
-		<section className="container mx-auto px-4 py-8 space-y-8">
+		<section className="container mx-auto px-4 py-8">
 			<PollQuestion poll={poll} />
-			<PollSubmissionForm poll={poll} />
+			<Title as="h2">Poll Responses</Title>
 
-			<div className="mt-4">
-				<SmallText>Total responses: {poll.responses.length}</SmallText>
-			</div>
+			<section className="space-y-4">
+				{poll.responses.map((response) => (
+					<div key={response.id}>
+						<Text>{response.selectedOptions.join(", ")}</Text>
+					</div>
+				))}
+			</section>
 		</section>
 	);
 }
