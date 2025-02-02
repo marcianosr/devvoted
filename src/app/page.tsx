da163dcf881e591dea88629d0a9ad69b5f491cef
Marciano Schildmeijer <marciano@kabisa.nl>
2025-02-02 10:06:34 +0100

    feat: get basic polls on /home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { createClient } from "@/app/supabase/client";
+import { PollWithDetails } from "@/types/db";
 
 export default async function Home() {
 	const supabase = createClient();
 
-	const { data: polls, ...rest } = await supabase.from("polls").select("*");
-	console.log({ polls, rest });
+	const { data: polls } = await supabase
+		.from("polls")
+		.select("*")
+		.returns<PollWithDetails[]>();
+
 	return <pre className="flex font-black">{JSON.stringify(polls)}</pre>;
 }
