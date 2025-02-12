e44303086f671960fb670c85cef596b4b57dab55
Marciano Schildmeijer <marciano@kabisa.nl>
2025-02-12 17:56:52 +0100

    refactor: remove poll submission for now

--- src/services/polls.ts
+++ src/services/polls.ts
@@ -1,8 +1,9 @@
-import { createClient } from "@/app/supabase/server";
-import { Poll } from "@/types/db";
+import { createClient as createServerClient } from "@/app/supabase/server";
+import { createClient as createBrowserClient } from "@/app/supabase/client";
+import { Poll, PollOption } from "@/types/db";
 
 export const getPoll = async (pollId: string): Promise<Poll | null> => {
-	const supabase = createClient();
+	const supabase = createServerClient();
 
 	const { data, error } = await (await supabase)
 		.from("polls")
@@ -16,4 +17,51 @@ export const getPoll = async (pollId: string): Promise<Poll | null> => {
 	}
 
 	return data;
+};
+
+export const getPollWithOptions = async (
+	pollId: string
+): Promise<{ poll: Poll | null; options: PollOption[] }> => {
+	const supabase = createServerClient();
+
+	const { data, error } = await (await supabase)
+		.from("polls")
+		.select("*, polls_options(*)")
+		.eq("id", pollId)
+		.single();
+
+	console.log(data, error);
+	if (error) {
+		console.error("Error fetching poll with options:", error);
+		return { poll: null, options: [] };
+	}
+
+	return { poll: data, options: data.polls_options || [] };
+};
+
+export const submitPollResponse = async (
+	pollId: string,
+	userId: string,
+	selectedOptions: string[]
+): Promise<{ success: boolean }> => {
+	const supabase = createBrowserClient();
+
+	const { error } = await supabase
+		.from("polls_responses")
+		.insert([
+			{
+				poll_id: pollId,
+				user_id: userId,
+				selected_options: selectedOptions,
+			},
+		])
+		.select()
+		.single();
+
+	if (error) {
+		console.error("Error submitting poll response:", error);
+		return { success: false };
+	}
+
+	return { success: true };
 };
