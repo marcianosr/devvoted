26b33cc699d524b70ba87f01088697b04d7df451
Marciano Schildmeijer <marciano@kabisa.nl>
2025-03-02 20:01:00 +0100

    refactor: multiplier xp and setup flow for good/wrong answer for poll

--- src/services/createPollResponse.ts
+++ src/services/createPollResponse.ts
@@ -1,7 +1,9 @@
 import { createClient } from "@/app/supabase/server";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { CreatePostPollResponseRequest } from "./api/createPostPollResponse";
-import { ActiveRun, UpdateActiveRun } from "@/types/db";
+import { ActiveRun, PollOption, UpdateActiveRun } from "@/types/db";
+import { calculateBetXP } from "./calculateXP";
+import { START_MULTIPLIER_INCREASE } from "./constants";
 
 // Not sure where to put this file, as it is inserting data triggerd by /api/submit-response
 export const createPollResponse = async (
@@ -28,9 +30,9 @@ export const createPollResponse = async (
 export const createPollResponseOptions = async (
 	supabase: SupabaseClient,
 	responseId: number,
-	selectedOptions: string[]
+	selectedOptions: PollOption[]
 ) => {
-	const responseOptions = selectedOptions.map((optionId: string) => ({
+	const responseOptions = selectedOptions.map((optionId: PollOption) => ({
 		response_id: responseId,
 		option_id: optionId,
 	}));
@@ -80,8 +82,6 @@ export const updateActiveRunByCategoryCode = async (
 	if (error) throw new Error(`Error creating active run: ${error.message}`);
 };
 
-export const DEFAULT_MULTIPLIER_INCREASE = 0.1;
-
 export const createPostPollResponse = async ({
 	poll,
 	userId,
@@ -101,34 +101,96 @@ export const createPostPollResponse = async ({
 			selectedOptions
 		);
 
-		// Get and update streak multiplier
-		const previousData = await getPreviousByCategoryCode(
-			userId,
-			poll.category_code
-		);
+		const hasIncorrectAnswer = true;
 
-		const currentMultiplier = Number(previousData?.streak_multiplier) || 0;
-		const newMultiplier = (
-			currentMultiplier + DEFAULT_MULTIPLIER_INCREASE
-		).toFixed(1);
-
-		// update and find active run
-		await updateActiveRunByCategoryCode(
-			{
-				user_id: userId,
-				temporary_xp: calculateXP(selectedBet),
-				streak_multiplier: newMultiplier,
-				last_poll_at: new Date(),
-				current_streak: previousData
-					? previousData.current_streak + 1
-					: 1,
-			},
-			poll.category_code
-		);
+		if (hasIncorrectAnswer) {
+			console.log("Incorrect answer");
+			// await handleWrongPollResponse({
+			// 	userId,
+			// 	categoryCode: poll.category_code,
+			// });
+			// return;
+		} else {
+			console.log("Correct answer");
+			// await handleCorrectPollResponse({
+			// 	selectedBet,
+			// 	userId,
+			// 	categoryCode: poll.category_code,
+			// });
+		}
 
 		return { success: true };
 	} catch (error) {
 		console.error("Error submitting poll response:", error);
 		throw error;
 	}
+};
+
+const handleCorrectPollResponse = async ({
+	selectedBet,
+	userId,
+	categoryCode,
+}: {
+	selectedBet: number;
+	userId: string;
+	categoryCode: string;
+}) => {
+	// Get and update streak multiplier
+	const previousData = await getPreviousByCategoryCode(userId, categoryCode);
+
+	const currentMultiplier = Number(previousData?.streak_multiplier) || 0;
+	const newMultiplier = (
+		currentMultiplier + START_MULTIPLIER_INCREASE
+	).toFixed(1);
+
+	const currentXP = previousData?.temporary_xp ?? 0;
+	const xpCalculation = calculateBetXP({
+		availableXP: currentXP,
+		betPercentage: selectedBet,
+		streakMultiplier: Number(newMultiplier),
+	});
+
+	// update and find active run
+	await updateActiveRunByCategoryCode(
+		{
+			user_id: userId,
+			temporary_xp: currentXP + xpCalculation.totalXP,
+			streak_multiplier: newMultiplier,
+			last_poll_at: new Date(),
+			current_streak: previousData ? previousData.current_streak + 1 : 0,
+		},
+		categoryCode
+	);
+};
+
+const handleWrongPollResponse = async ({
+	userId,
+	categoryCode,
+}: {
+	userId: string;
+	categoryCode: string;
+}) => {
+	await resetActiveRunByCategoryCode(userId, categoryCode);
+};
+
+const resetActiveRunByCategoryCode = async (
+	userId: string,
+	categoryCode: string
+) => {
+	const supabase = await createClient();
+
+	const { error } = await supabase
+		.from("polls_active_runs")
+		.update({
+			temporary_xp: 0,
+			streak_multiplier: "0.0",
+			last_poll_at: new Date(),
+			current_streak: 0,
+		})
+		.eq("category_code", categoryCode)
+		.eq("user_id", userId);
+
+	if (error) throw new Error(`Error resetting active run: ${error.message}`);
+
+	return { success: true };
 };
